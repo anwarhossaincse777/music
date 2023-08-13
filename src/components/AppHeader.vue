2f9f22c765b1311ec7825bc24529dbb70c4ad4a3
Betabloker <72620275+Betabloker@users.noreply.github.com>
2023-08-13 15:50:25 +0600

    updated authcomponent

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,7 @@
 <script setup>
+    import {useModalStore} from "@/stores/modal";
 
+    const modalStore =useModalStore()
 </script>
 
 <template>
@@ -15,7 +17,7 @@
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
           <li>
-            <a class="px-2 text-white" href="#">Login / Register</a>
+            <a class="px-2 text-white" href="#" @click.prevent="modalStore.toggleAuthModal()">Login / Register</a>
           </li>
           <li>
             <a class="px-2 text-white" href="#">Manage</a>
---
+++ src/stores/modal.js
@@ -0,0 +1,22 @@
+
+import { defineStore } from 'pinia'
+
+
+
+export const useModalStore = defineStore('modal', {
+    state: () => ({
+        authModalShow: true
+
+    }),
+    getters: {
+
+    },
+    actions: {
+
+        toggleAuthModal() {
+
+            return this.authModalShow = !this.authModalShow;
+
+        },
+    }
+})
